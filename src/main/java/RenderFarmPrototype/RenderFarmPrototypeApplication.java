b564a805107f5917aceab9feaf5b8dee937f7c27
Mikhail <mcmishabest@gmail.com>
2022-01-27 13:52:09 +0300

    Initial commit

---
+++ src/main/java/RenderFarmPrototype/RenderFarmPrototypeApplication.java
@@ -0,0 +1,13 @@
+package RenderFarmPrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderFarmPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderFarmPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RenderFarmPrototype/RenderFarmPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package RenderFarmPrototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderFarmPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
